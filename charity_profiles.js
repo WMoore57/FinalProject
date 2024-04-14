f4ded0f78e7fea4ee4e4e247fc96aa1b84f277b3
WMoore57 <156708633+WMoore57@users.noreply.github.com>
2024-04-14 16:13:59 -0500

    Update charity_profiles.js

--- charity_profiles.js
+++ charity_profiles.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-            var volunteerButton = document.getElementById("volunteerButton");
-            var volunteerForm = document.getElementById("volunteerForm");
+    var volunteerButton = document.getElementById("volunteerButton");
+    var volunteerForm = document.getElementById("volunteerForm");
 
-            volunteerButton.addEventListener("click", function(event) {
-                event.preventDefault();
-                volunteerForm.style.display = "block";
-            });
-        });
+    volunteerButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        volunteerForm.style.display = "block";
+    });
 
-function shareOnSocialMedia(platform) {
-    var currentURL = window.location.href;
-    var shareText = "Check out Pheasants Forever - Charity Profile!";
-    var shareURL;
-    switch (platform) {
-        case 'facebook':
-            shareURL = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(currentURL);
-            break;
-        case 'twitter':
-            shareURL = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(currentURL) + '&text=' + encodeURIComponent(shareText);
-            break;
-        default:
-            console.error('Unsupported social media platform');
-            return;
+    function shareOnSocialMedia(platform) {
+        var currentURL = window.location.href;
+        var shareText = "Check out Pheasants Forever - Charity Profile!";
+        var shareURL;
+        switch (platform) {
+            case 'facebook':
+                shareURL = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(currentURL);
+                break;
+            case 'twitter':
+                shareURL = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(currentURL) + '&text=' + encodeURIComponent(shareText);
+                break;
+            default:
+                console.error('Unsupported social media platform');
+                return;
+        }
+        window.open(shareURL, '_blank');
     }
-    window.open(shareURL, '_blank');
+});
