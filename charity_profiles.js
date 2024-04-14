4ffdf35f9c41637d6f53fa90f1f7ada654e942f4
WMoore57 <156708633+WMoore57@users.noreply.github.com>
2024-04-14 16:07:35 -0500

    Update charity_profiles.js

--- charity_profiles.js
+++ charity_profiles.js
@@ -7,3 +7,20 @@ document.addEventListener("DOMContentLoaded", function() {
                 volunteerForm.style.display = "block";
             });
         });
+
+function shareOnSocialMedia(platform) {
+    var currentURL = window.location.href;
+    var shareText = "Check out Pheasants Forever - Charity Profile!";
+    var shareURL;
+    switch (platform) {
+        case 'facebook':
+            shareURL = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(currentURL);
+            break;
+        case 'twitter':
+            shareURL = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(currentURL) + '&text=' + encodeURIComponent(shareText);
+            break;
+        default:
+            console.error('Unsupported social media platform');
+            return;
+    }
+    window.open(shareURL, '_blank');
